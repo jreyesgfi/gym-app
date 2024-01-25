3d4ec60e10ec037d8aec585da10c19635632df35
jorge <91345902+jreyesgfi@users.noreply.github.com>
2024-01-25 21:08:44 +0200

    enhance the connection method

--- src/connection/connectionMethods.tsx
+++ src/connection/connectionMethods.tsx
@@ -1,5 +1,5 @@
-import { connectionUrl, connectionData } from "./connectionConfig";
 import axios from 'axios';
+import { connectionUrl, connectionData } from "./connectionConfig";
 
 // ----------------------------------------------------------------
 export const submitData = (): void => {
@@ -7,13 +7,13 @@ export const submitData = (): void => {
     const params = Object.fromEntries(urlSearchParams.entries());
     const { ID, token } = params; // Assuming 'ID' is the query parameter name
 
-    const data = {
-        ID: ID,
+    const postData = {
+        id: ID,
         token: token,
-        Method: "addData" // Assuming 'addData' is your method identifier
-    }
+        method: "addData" // Assuming 'addData' is your method identifier
+    };
 
-    axios.post(connectionUrl, data, connectionData)
+    axios.post(connectionUrl, postData, connectionData)
         .then(response => {
             console.log('Response:', response.data);
         })
@@ -23,20 +23,24 @@ export const submitData = (): void => {
 };
 
 //----------------------------------------------------------------
-export const receiveData = (callback: (data: string) => void): void => {
+export const receiveData = (callback: (data: any) => void): void => {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
     const { ID, token } = params; // Assuming 'ID' is the query parameter name
 
-    const data = {
-        ID: ID,
+    const getData = {
+        id: ID,
         token: token,
-        Method: "getData" // Assuming 'getData' is your method identifier
-    }
+        method: "getData" // Assuming 'getData' is your method identifier
+    };
 
-    axios.post(connectionUrl, data, connectionData)
-        .then((response) => {
-            callback(response.data);
+    axios.post(connectionUrl, getData, connectionData)
+        .then(response => {
+            // You may want to process the data before passing to the callback
+            const { muscles, who } = JSON.parse(response.data);
+            console.log("Muscles:", muscles); // Process the muscles array
+            console.log("Who:", who); // Process the who array
+            callback({ muscles, who });
         })
         .catch((error) => {
             console.error('Error:', error);
